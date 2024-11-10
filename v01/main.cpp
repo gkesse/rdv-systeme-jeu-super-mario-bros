42d885831a8271cae1f6300d66bf450b837b4a32
Gerard KESSE <tiakagerard@hotmail.com>
2024-11-10 02:37:40 +0100

    Configuration de l’écran d’accueil en C++ avec Qt

--- v01/main.cpp
+++ v01/main.cpp
@@ -1,11 +1,21 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/images/icon"));
+
+    QSplashScreen splash;
+    splash.setPixmap(QPixmap(":/images/mariosplash.png"));
+    splash.show();
+
     QMainWindow mainWin;
-    mainWin.show();
+
+    QTimer::singleShot(500, &splash, SLOT(close()));
+    QTimer::singleShot(500, &mainWin, SLOT(show()));
+
     return a.exec();
 }
