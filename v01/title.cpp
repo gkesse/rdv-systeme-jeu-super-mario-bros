f1f09e23777defbd1a7b793c540f067746119049
Gerard KESSE <tiakagerard@hotmail.com>
2024-11-17 22:59:27 +0100

    Conception de la scène de titre de l’application

--- v01/title.cpp
+++ v01/title.cpp
@@ -3,10 +3,19 @@
 
 #include <QScrollBar>
 #include <QPropertyAnimation>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QRadioButton>
+#include <QFontDatabase>
+#include <QApplication>
 
 Title::Title(View *view, QWidget *parent)
     : QGraphicsScene(parent)
 {
+    setFocus();
+    setSceneRect(0, 0, 1280, 720);
+
     viewer = view;
 
     viewer->setViewportUpdateMode(QGraphicsView::SmartViewportUpdate);
@@ -21,11 +30,105 @@ Title::Title(View *view, QWidget *parent)
     animation->start();
     addItem(background);
 
+    foreground = new QGraphicsPixmapItem(QPixmap(":/images/title.png"));
+    addItem(foreground);
+
+    logo = new QGraphicsPixmapItem(QPixmap(":/images/logo.png"));
+    logo->setPos((width - logo->boundingRect().width()) / 2, height / 12);
+    addItem(logo);
+
     viewer->sceneSet(this);
+
+    int id = QFontDatabase::addApplicationFont(":/font/mvboli.ttf");
+    QFontDatabase::applicationFontFamilies(id);
+
+    QFont font("MV Boli", 15, QFont::Bold);
+
+    userName = new QLabel(viewer);
+    userName->setFont(font);
+    userName->setText("Username");
+    userName->setObjectName(QString("username"));
+    userName->setGeometry(QRect(430, 420, 100, 25));
+
+    userLine = new QLineEdit(viewer);
+    userLine->setToolTip("Enter an email address");
+    userLine->setGeometry(QRect(540, 420, 200, 25));
+
+    password = new QLabel(viewer);
+    password->setFont(font);
+    password->setText("Password");
+    password->setObjectName(QString("password"));
+    password->setGeometry(QRect(430, 450, 100, 25));
+
+    passLine = new QLineEdit(viewer);
+    passLine->setEchoMode(QLineEdit::Password);
+    passLine->setObjectName(QString("passLine"));
+    passLine->setToolTip("Enter password");
+    passLine->setGeometry(QRect(540, 450, 200, 25));
+
+    radioButton = new QRadioButton(viewer);
+    radioButton->setObjectName(QString("radioButton"));
+    radioButton->setToolTip("Click to show password text");
+    radioButton->setGeometry(QRect(760, 450, 100, 25));
+    connect(radioButton, SIGNAL(toggled(bool)), this, SLOT(on_radioButton_toggled(bool)));
+
+    radioText = new QLabel(viewer);
+    radioText->setText("Show Password");
+    radioText->setToolTip("Click to show password text");
+    radioText->setGeometry(QRect(780, 450, 100, 25));
+
+    loginButton = new QPushButton(viewer);
+    loginButton->setText("Login");
+    loginButton->setObjectName(QString("loginButton"));
+    loginButton->setToolTip("Click to login");
+    loginButton->setGeometry(QRect(540, 500, 100, 32));
+    connect(loginButton, SIGNAL(clicked()), this, SLOT(login()));
+
+    newUserButton = new QPushButton(viewer);
+    newUserButton->setText("New User");
+    newUserButton->setObjectName(QString("newUserButton"));
+    newUserButton->setToolTip("Click to create a login");
+    newUserButton->setGeometry(QRect(642, 500, 100, 32));
+    connect(newUserButton, SIGNAL(clicked()), this, SLOT(newUser()));
+
+    developerButton = new QPushButton(viewer);
+    developerButton->setText("Guest Login");
+    developerButton->setObjectName(QString("developerButton"));
+    developerButton->setToolTip("Login as a guest");
+    developerButton->setGeometry(QRect(540, 535, 100, 32));
+    connect(developerButton, SIGNAL(clicked()), this, SLOT(developerLogin()));
+
+    quitButton = new QPushButton(viewer);
+    quitButton->setText("Quit");
+    quitButton->setObjectName(QString("quitButton"));
+    quitButton->setToolTip("Quit program");
+    quitButton->setGeometry(QRect(642, 535, 100, 32));
+    connect(quitButton, SIGNAL(clicked()), this, SLOT(quitProgram()));
 
     scroll = viewer->horizontalScrollBar();
 }
 
 Title::~Title()
 {
+}
+
+void Title::on_radioButton_toggled(bool checked)
+{
+}
+
+void Title::login()
+{
+}
+
+void Title::newUser()
+{
+}
+
+void Title::developerLogin()
+{
+}
+
+void Title::quitProgram()
+{
+    qApp->quit();
 }
--- v01/title.h
+++ v01/title.h
@@ -5,6 +5,10 @@
 
 class QScrollBar;
 class QPropertyAnimation;
+class QPushButton;
+class QLineEdit;
+class QLabel;
+class QRadioButton;
 
 class View;
 class AnimatedGraphicsItem;
@@ -17,13 +21,35 @@ public:
     explicit Title(View *view, QWidget *parent = 0);
     ~Title();
 
+private slots:
+    void on_radioButton_toggled(bool checked);
+    void login();
+    void newUser();
+    void developerLogin();
+    void quitProgram();
+
 private:
     const int width = 1280;
+    const int height = 720;
 
     View *viewer;
 
     AnimatedGraphicsItem *background;
     QPropertyAnimation *animation;
+    QGraphicsPixmapItem *foreground;
+    QGraphicsPixmapItem *logo;
+
+    QLabel *userName;
+    QLineEdit *userLine;
+    QLabel *password;
+    QLineEdit *passLine;
+    QLabel *radioText;
+    QRadioButton *radioButton;
+
+    QPushButton *loginButton;
+    QPushButton *developerButton;
+    QPushButton *quitButton;
+    QPushButton *newUserButton;
 
     QScrollBar *scroll;
 };
